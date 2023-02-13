69972f5c31264b56b56fac7285d633e39e932352
Vladislav Polekha <vladsimplemente@gmail.com>
2023-02-13 21:19:38 +0300

    core: template git clean up

---
+++ .storybook/manager.js
@@ -0,0 +1,9 @@
+import { themes } from '@storybook/theming';
+import { addons } from '@storybook/addons';
+
+addons.setConfig({
+  theme: {
+    ...themes.dark,
+    brandTitle: 'Components',
+  },
+});
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { styled } from '@linaria/react';
+
+const StyledButton = styled.button`
+  color: red;
+`;
+
+const App = () => (
+  <div>
+    text
+    <StyledButton>Button</StyledButton>
+  </div>
+);
+
+export default App;
